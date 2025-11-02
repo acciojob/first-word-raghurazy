b4ad2e27b4eec7c1b9cf8437b852f762c261d9dc
raghurazy <135259028+raghurazy@users.noreply.github.com>
2025-11-02 17:30:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function firstWord(s) {
-  // your code here
+  const words = s.trim().split(/\s+/);
+  return words[0] || ""; 
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
